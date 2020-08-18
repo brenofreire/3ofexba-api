f872cc62a7d7c921bf1e607c495102f71cdaf35a
Breno Freire <brenofreire.dev@gmail.com>
2020-08-17 22:41:18 -0300

    chore: improves db seeds

--- database/seeders/UsuariosAdmin.ts
+++ database/seeders/UsuariosAdmin.ts
@@ -2,7 +2,7 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Usuario from 'App/Models/Usuario'
 
 export default class UsuariosAdminSeeder extends BaseSeeder {
-  public async run () {
+  public async run() {
     await Usuario.updateOrCreateMany('idDemolay', [{
       email: 'breno.loorran@gmail.com',
       password: '123123123',
@@ -10,20 +10,30 @@ export default class UsuariosAdminSeeder extends BaseSeeder {
       role: 'admin',
       status: 1,
       idDemolay: 38938,
-    },{
+    }, {
       email: 'email@teste.com',
       password: '123123123',
       capitulo: 99,
       role: 'comum',
       status: 1,
       idDemolay: 1,
+      cargo: 'mc',
     }, {
       email: 'email2@teste.com',
+      password: '123123123',
+      capitulo: 99,
+      role: 'comum',
+      status: 1,
+      idDemolay: 2,
+      cargo: 'esc',
+    }, {
+      email: 'email3@teste.com',
       password: '123123123',
       capitulo: 772,
       role: 'comum',
       status: 1,
-      idDemolay: 2,
+      idDemolay: 3,
+      cargo: 'mc',
     }])
   }
 }
